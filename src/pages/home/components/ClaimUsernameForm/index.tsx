2daf0c586c42516054b3c41d17cf83f9dd50094a
hilquiasfmelo <hilquiasfmelo@hotmail.com>
2022-12-28 11:40:04 -0300

    🚀 CU: Formulário do cadastroa - Ignite Call

---
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -0,0 +1,62 @@
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+import { Button, Text, TextInput } from '@ignite-ui/react'
+import { ArrowRight } from 'phosphor-react'
+
+import { Form, FormAnnotation } from './styles'
+
+const claimUsernameFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'O usuário precisa ter pelo menos 3 letras' })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: 'O usuário deve ter apenas letras e hifens',
+    })
+    .transform((username) => username.toLowerCase()),
+})
+
+type claimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
+
+export function ClaimUsernameForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<claimUsernameFormData>({
+    resolver: zodResolver(claimUsernameFormSchema),
+  })
+
+  async function handleClaimUsername(data: claimUsernameFormData) {
+    console.log(data)
+  }
+
+  return (
+    <>
+      <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+        <TextInput
+          size="sm"
+          prefix="ignite.com/"
+          placeholder="seu-usuario"
+          {...register('username')}
+        />
+        <Button size="sm" type="submit">
+          Reservar
+          <ArrowRight />
+        </Button>
+      </Form>
+
+      {errors.username ? (
+        <FormAnnotation>
+          <Text size="sm" style={{ color: '#F75A68' }}>
+            {errors.username.message}
+          </Text>
+        </FormAnnotation>
+      ) : (
+        <FormAnnotation>
+          <Text size="sm">Digite o nome do usuário desejado</Text>
+        </FormAnnotation>
+      )}
+    </>
+  )
+}
---
+++ src/pages/register/index.page.tsx
@@ -0,0 +1,81 @@
+import { useForm } from 'react-hook-form'
+import { ArrowRight } from 'phosphor-react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+
+import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
+import { Container, Form, FormError, Header } from './styles'
+
+const registerFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'O usuário precisa ter pelo menos 3 letras' })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: 'O usuário deve ter apenas letras e hifens',
+    })
+    .transform((username) => username.toLowerCase()),
+  name: z
+    .string()
+    .min(3, { message: 'O nome precisa ter pelo menos 3 letras' }),
+})
+
+type registerFormData = z.infer<typeof registerFormSchema>
+
+export default function Register() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<registerFormData>({
+    resolver: zodResolver(registerFormSchema),
+  })
+
+  async function handleRegister(data: registerFormData) {
+    console.log(data)
+  }
+
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Bem-vindo ao Ignite Call!</Heading>
+        <Text size="sm">
+          Precisamos de algumas informações para criar seu perfil! Ah, você pode
+          editar essas informações depois.
+        </Text>
+
+        <MultiStep size={4} currentStep={1} />
+      </Header>
+
+      <Form as="form" onSubmit={handleSubmit(handleRegister)}>
+        <label>
+          <Text size="sm">Nome de usuário</Text>
+          <TextInput
+            prefix="ignite.com/"
+            placeholder="seu-usuario"
+            {...register('username')}
+          />
+
+          {/* triggers a message if there is an error  */}
+          {errors.username && (
+            <FormError size="sm">{errors.username.message}</FormError>
+          )}
+        </label>
+
+        <label>
+          <Text size="sm">Nome de completo</Text>
+          <TextInput placeholder="Seu nome" {...register('name')} />
+
+          {/* triggers a message if there is an error  */}
+          {errors.name && (
+            <FormError size="sm">{errors.name.message}</FormError>
+          )}
+        </label>
+
+        <Button type="submit" disabled={isSubmitting}>
+          Próximo passo
+          <ArrowRight />
+        </Button>
+      </Form>
+    </Container>
+  )
+}
