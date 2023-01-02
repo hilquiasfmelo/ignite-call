4947abe383bca408c3bd06351bd02fedd6ac8b61
hilquiasfmelo <hilquiasfmelo@hotmail.com>
2023-01-02 17:58:29 -0300

    🚀 Task: Página de confirmaçãol - Ignite Call

---
+++ src/pages/schedule/[username]/schedule-form/index.page.tsx
@@ -0,0 +1,9 @@
+// import { CalendarStep } from './calendar-step'
+import { ConfirmStep } from './confirm-step'
+
+export function ScheduleForm() {
+  return (
+    // <CalendarStep />
+    <ConfirmStep />
+  )
+}
