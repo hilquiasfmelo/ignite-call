6051089eaf7739574691ec36ef7a7cf403286b43
hilquiasfmelo <hilquiasfmelo@hotmail.com>
2023-01-07 11:53:59 -0300

    🚀 Create builde

--- src/pages/schedule/[username]/index.page.tsx
+++ src/pages/schedule/[username]/index.page.tsx
@@ -2,7 +2,7 @@ import { Avatar, Heading, Text } from '@ignite-ui/react'
 import { GetStaticPaths, GetStaticProps } from 'next'
 import { NextSeo } from 'next-seo'
 import { prisma } from '../../../lib/prisma'
-import { ScheduleForm } from './schedule-form/index.page'
+import ScheduleForm from './schedule-form/index.page'
 
 import { Container, UserHeader } from './styles'
 
--- src/pages/schedule/[username]/schedule-form/index.page.tsx
+++ src/pages/schedule/[username]/schedule-form/index.page.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { CalendarStep } from './calendar-step'
 import { ConfirmStep } from './confirm-step'
 
-export function ScheduleForm() {
+export default function ScheduleForm() {
   const [selectedDateTime, setSelectedDateTime] = useState<Date | null>()
 
   function handleClearSelectedDateTime() {
